e60e6c1b716dd5d9d662dd1ff6aecd8c411e41c3
vnatyreromchiss <rdg2039@gmail.com>
2022-11-03 18:03:52 -0500

    master

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if(reviewsSwiper) {
+    const swiper = new Swiper('.swiper-reviews', {
+        // Optional parameters
+        loop: true,
+        autoHeight:true,
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+      });
+}
